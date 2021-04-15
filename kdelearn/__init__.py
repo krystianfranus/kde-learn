7ed9e8d047bb7455d243b939d751ed8896829058
Krystian Franus <krystian.franus@gmail.com>
2021-04-15 15:26:00 +0200

    setup flake8

--- kdelearn/__init__.py
+++ kdelearn/__init__.py
@@ -1,2 +1,8 @@
-from .kernels import *
 from .kde import Kde
+from .kernels import uniform, gaussian, epanechnikov, cauchy
+
+__all__ = ['uniform',
+           'gaussian',
+           'epanechnikov',
+           'cauchy',
+           'Kde']
