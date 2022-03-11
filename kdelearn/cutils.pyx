1cd710b500feed04793196a39494d1f62429caaa
Krystian Franus <krystian.franus@gmail.com>
2022-03-11 20:04:50 +0100

    Add uniform kernel

--- kdelearn/cutils.pyx
+++ kdelearn/cutils.pyx
@@ -2,20 +2,33 @@ cimport cython
 
 import numpy as np
 
-from libc.math cimport exp, pi, sqrt
+from libc.math cimport abs, exp, pi, sqrt
 
 
 cdef double gaussian(double x):
     return 1 / sqrt(2 * pi) * exp(-0.5 * x**2)
 
 
+cdef double uniform(double x):
+    if abs(x) <= 1.0:
+        return 0.5
+    else:
+        return 0.0
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth):
+def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth, str kernel_name):
     cdef Py_ssize_t m_train = x_train.shape[0]
     cdef Py_ssize_t m_test = x_test.shape[0]
     cdef Py_ssize_t n = x_train.shape[1]
+
+    if kernel_name == "gaussian":
+        kernel = gaussian
+    elif kernel_name == "uniform":
+        kernel = uniform
+    else:
+        raise RuntimeError("Invalid kernel name")
 
     scores = np.zeros(m_test, dtype=np.float64)
     cdef double[:] scores_view = scores
@@ -27,7 +40,7 @@ def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth):
         for i in range(m_train):
             tmp = 1.0
             for j in range(n):
-                tmp *= gaussian((x_test[k, j] - x_train[i, j]) / bandwidth[j])
+                tmp *= kernel((x_test[k, j] - x_train[i, j]) / bandwidth[j])
             scores_view[k] += tmp
         scores_view[k] *= 1 / (m_train * np.prod(bandwidth))
     return scores
