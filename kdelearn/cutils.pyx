6b03601e44c33abf67bd0e2ed613960e5ea3b83e
Krystian Franus <krystian.franus@gmail.com>
2022-03-11 23:22:15 +0100

    Add epanechnikov kernel

--- kdelearn/cutils.pyx
+++ kdelearn/cutils.pyx
@@ -15,10 +15,17 @@ cdef double uniform(double x):
     else:
         return 0.0
 
+
+cdef double epanechnikov(double x):
+    if abs(x) <= 1.0:
+        return 3.0 / 4.0 * (1.0 - x**2.0)
+    else:
+        return 0.0
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth, str kernel_name):
+def compute_kde(double[:, :] x_train, double[:] weights_train, double[:] bandwidth, double[:, :] x_test, str kernel_name):
     cdef Py_ssize_t m_train = x_train.shape[0]
     cdef Py_ssize_t m_test = x_test.shape[0]
     cdef Py_ssize_t n = x_train.shape[1]
@@ -27,8 +34,10 @@ def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth, 
         kernel = gaussian
     elif kernel_name == "uniform":
         kernel = uniform
+    elif kernel_name == "epanechnikov":
+        kernel = epanechnikov
     else:
-        raise RuntimeError("Invalid kernel name")
+        raise RuntimeError("invalid kernel name")
 
     scores = np.zeros(m_test, dtype=np.float64)
     cdef double[:] scores_view = scores
@@ -40,7 +49,6 @@ def compute_kde(double[:, :] x_train, double[:, :] x_test, double[:] bandwidth, 
         for i in range(m_train):
             tmp = 1.0
             for j in range(n):
-                tmp *= kernel((x_test[k, j] - x_train[i, j]) / bandwidth[j])
-            scores_view[k] += tmp
-        scores_view[k] *= 1 / (m_train * np.prod(bandwidth))
+                tmp *= 1.0 / bandwidth[j] * kernel((x_test[k, j] - x_train[i, j]) / bandwidth[j])
+            scores_view[k] += weights_train[i] * tmp
     return scores
