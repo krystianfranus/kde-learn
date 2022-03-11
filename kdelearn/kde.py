7e63d215dfff5bd69afc6f0a01e47c98fd3aaae0
Krystian Franus <krystian.franus@gmail.com>
2022-03-12 00:12:59 +0100

    Refactor Kde

--- kdelearn/kde.py
+++ kdelearn/kde.py
@@ -9,10 +9,15 @@ from .utils import estimate_bandwidth
 
 
 class Kde:
-    def __init__(self, kernel_name: str):
+    def __init__(self, kernel_name: str = "gaussian"):
         self.kernel_name = kernel_name
 
-    def fit(self, x_train: ndarray, weights_train: Optional[ndarray] = None):
+    def fit(
+        self,
+        x_train: ndarray,
+        weights_train: Optional[ndarray] = None,
+        bandwidth: Optional[ndarray] = None,
+    ):
         if len(x_train.shape) != 2:
             raise RuntimeError("x_train must be 2d ndarray")
         self.x_train = np.copy(x_train)
@@ -24,14 +29,20 @@ class Kde:
             if len(weights_train.shape) != 1:
                 raise RuntimeError("weights_train must be 1d ndarray")
             if not (weights_train > 0).all():
-                raise RuntimeError("weights_train must be positive")
+                raise ValueError("weights_train must be positive")
             self.weights_train = np.copy(weights_train)
             self.weights_train = self.weights_train / self.weights_train.sum()
 
-        self.bandwidth = estimate_bandwidth(
-            self.x_train,
-            kernel_name=self.kernel_name,
-        )
+        if bandwidth is None:
+            self.bandwidth = estimate_bandwidth(
+                self.x_train,
+                self.kernel_name,
+            )
+        else:
+            if not (bandwidth > 0).all():
+                raise ValueError("bandwidth must be positive")
+            self.bandwidth = np.copy(bandwidth)
+
         return self
 
     def score_samples(self, x_test: ndarray) -> ndarray:
