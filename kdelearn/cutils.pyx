2d0c2fe048620b7d1b5ad6dd317ef1cd9c340048
Krystian Franus <krystian.franus@gmail.com>
2022-03-14 19:44:25 +0100

    Add docstrings in cutils

--- kdelearn/cutils.pyx
+++ kdelearn/cutils.pyx
@@ -1,30 +1,76 @@
 cimport cython
 
 import numpy as np
+from numpy import ndarray
 
 from libc.math cimport abs, exp, pi, sqrt
 
 
-cdef double gaussian(double x):
+cdef double cgaussian(double x):
     return 1.0 / sqrt(2.0 * pi) * exp(-0.5 * x ** 2)
 
 
-cdef double uniform(double x):
+# wrapper to expose cgaussian() to Python
+def gaussian(x: float) -> float:
+    """
+    Gaussian kernel function.
+
+    .. math::
+        K(x) = \\frac{1}{\\sqrt{2 \\pi}} \\exp \\left( \\frac{x^2}{2} \\right)
+    """
+    return cgaussian(x)
+
+
+cdef double cuniform(double x):
     if abs(x) <= 1.0:
         return 0.5
     else:
         return 0.0
 
 
-cdef double epanechnikov(double x):
+# wrapper to expose cuniform() to Python
+def uniform(x: float) -> float:
+    """
+    Uniform kernel function.
+
+    .. math::
+        K(x) = 1 \\quad \\text{if } |x| \\leq 0 \\quad  \\text{else } 0
+    """
+    return cuniform(x)
+
+
+cdef double cepanechnikov(double x):
     if abs(x) <= 1.0:
         return 3.0 / 4.0 * (1.0 - x ** 2.0)
     else:
         return 0.0
 
 
-cdef double cauchy(double x):
+# wrapper to expose cepanechnikov() to Python
+def epanechnikov(x: float) -> float:
+    """
+    Epanechnikov kernel function.
+
+    .. math::
+        K(x) = \\frac{3}{4} (1-x^2) \\quad \\text{if } |x| \\leq 0 \\quad  \\text{else } 0
+    """
+    return cepanechnikov(x)
+
+
+cdef double ccauchy(double x):
     return 2.0 / (pi * (x ** 2 + 1.0) ** 2)
+
+
+# wrapper to expose ccauchy() to Python
+def cauchy(x: float) -> float:
+    """
+    Cauchy kernel function.
+
+    .. math::
+        K(x) = \\frac{2}{\\pi (x^2 + 1)^2}
+    """
+    return ccauchy(x)
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -50,13 +96,13 @@ def compute_kde(
     cdef Py_ssize_t n = x_train.shape[1]
 
     if kernel_name == "gaussian":
-        kernel = gaussian
+        kernel = cgaussian
     elif kernel_name == "uniform":
-        kernel = uniform
+        kernel = cuniform
     elif kernel_name == "epanechnikov":
-        kernel = epanechnikov
+        kernel = cepanechnikov
     elif kernel_name == "cauchy":
-        kernel = cauchy
+        kernel = ccauchy
     else:
         raise RuntimeError("invalid kernel name")
 
