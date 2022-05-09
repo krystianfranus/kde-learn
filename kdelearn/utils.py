40c0d1b32743a3af73eaafd4edc2118a13f49bf1
Krystian Franus <krystian.franus@gmail.com>
2022-05-09 21:34:47 +0200

    Add plug-in method

--- kdelearn/utils.py
+++ kdelearn/utils.py
@@ -41,6 +41,55 @@ def scotts_rule(x_train: ndarray, kernel_name: str = "gaussian") -> ndarray:
     return bandwidth
 
 
+def estimate_bandwidth_plugin(x_train: ndarray, rank: int):
+    """Gaussian kernel specific."""
+    if rank > 3:
+        raise NotImplementedError("rank can't be greater than 3")
+    wk, uk = 1 / (2 * np.sqrt(np.pi)), 1
+
+    m_train = x_train.shape[0]
+    std_x = np.std(x_train, axis=0, ddof=1)
+
+    # fmt: off
+    def _c(std_x, ksi):
+        nominator = np.math.factorial(ksi) * (-1) ** (0.5 * ksi)
+        denominator = np.math.factorial(0.5 * ksi) * np.sqrt(np.pi) * (2 * std_x) ** (ksi + 1)
+        return nominator / denominator
+
+    def _h(ksi, gaussian_derivative, uk, c, m_train):
+        return (-2 * gaussian_derivative(0) / (uk * c * m_train)) ** (1 / (ksi + 1))
+
+    def _C(x_train, ksi, h, gaussian_derivative, m_train):
+        return 1 / (m_train ** 2 * h ** (ksi + 1)) * np.sum(np.sum(gaussian_derivative((x_train[:, None] - x_train) / h), axis=0), axis=0)
+
+    def gaussian_derivative_of_order_4(x):
+        return 1 / np.sqrt(2 * np.pi) * (x ** 4 - 6 * x ** 2 + 3) * np.exp(-0.5 * x ** 2)
+
+    def gaussian_derivative_of_order_6(x):
+        return 1 / np.sqrt(2 * np.pi) * (x ** 6 - 15 * x ** 4 + 45 * x ** 2 - 15) * np.exp(-0.5 * x ** 2)
+
+    def gaussian_derivative_of_order_8(x):
+        return 1 / np.sqrt(2 * np.pi) * (x ** 8 - 28 * x ** 6 + 210 * x ** 4 - 420 * x ** 2 + 105) * np.exp(-0.5 * x ** 2)
+    # fmt: on
+
+    ksi = 2 * rank + 4
+    C = _c(std_x, ksi)
+    funcs = {
+        "func8": gaussian_derivative_of_order_8,
+        "func6": gaussian_derivative_of_order_6,
+        "func4": gaussian_derivative_of_order_4,
+    }
+    while ksi != 4:
+        gaussian_derivative = funcs[f"func{ksi - 2}"]
+        h = _h(ksi, gaussian_derivative, uk, C, m_train)
+        C = _C(x_train, ksi - 2, h, gaussian_derivative, m_train)
+        ksi -= 2
+    zf = C
+
+    bandwidth = (wk / (uk ** 2 * zf * m_train)) ** (1 / 5)
+    return bandwidth
+
+
 def accuracy(labels_true: ndarray, labels_pred: ndarray) -> float:
     """Accuracy score computes fraction of correctly classified samples.
 
