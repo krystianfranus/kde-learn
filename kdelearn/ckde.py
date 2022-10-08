7f33552a0b3971addd66cc0a579127aba14167f3
Krystian Franus <krystian.franus@gmail.com>
2022-10-08 19:54:56 +0200

    Fix

--- kdelearn/ckde.py
+++ kdelearn/ckde.py
@@ -27,12 +27,12 @@ class CKDE:
     --------
     >>> # Prepare data
     >>> m_train = 100
-    >>> n_x, n_w = 1, 1
+    >>> n_x, n_y = 1, 1
     >>> x_train = np.random.normal(0, 1, size=(m_train, n_x))
-    >>> w_train = np.random.normal(0, 1, size=(m_train, n_w))
-    >>> w_star = np.array([0.0] * n_w)
+    >>> y_train = np.random.normal(0, 1, size=(m_train, n_y))
+    >>> y_star = np.array([0.0] * n_y)
     >>> # Fit
-    >>> ckde = CKDE("gaussian").fit(x_train, w_train, w_star)
+    >>> ckde = CKDE("gaussian").fit(x_train, y_train, y_star)
     """
 
     def __init__(self, kernel_name: str = "gaussian"):
@@ -45,11 +45,11 @@ class CKDE:
     def fit(
         self,
         x_train: ndarray,
-        w_train: ndarray,
-        w_star: ndarray,
+        y_train: ndarray,
+        y_star: ndarray,
         weights_train: Optional[ndarray] = None,
         bandwidth_x: Optional[ndarray] = None,
-        bandwidth_w: Optional[ndarray] = None,
+        bandwidth_y: Optional[ndarray] = None,
         bandwidth_method: str = "normal_reference",
         **kwargs,
     ):
@@ -60,16 +60,16 @@ class CKDE:
         x_train : ndarray of shape (m_train, n_x)
             Data points (describing variables) as an array containing data with float
             type.
-        w_train : ndarray of shape (m_train, n_w)
+        y_train : ndarray of shape (m_train, n_y)
             Data points (conditioning variables) as an array containing data with float
             type.
-        w_star : ndarray of shape (n_w,)
+        y_star : ndarray of shape (n_y,)
             Conditioned value.
         weights_train : ndarray of shape (m_train,), optional
             Weights of data points. If None, all points are equally weighted.
         bandwidth_x : ndarray of shape (n_x,), optional
             Smoothing parameter of describing variables.
-        bandwidth_w : ndarray of shape (n_w,), optional
+        bandwidth_y : ndarray of shape (n_y,), optional
             Smoothing parameter of conditioning variables.
         bandwidth_method : {'normal_reference', 'direct_plugin', 'ste_plugin', \
                 'ml_cv'}, default='normal_reference'
@@ -85,15 +85,15 @@ class CKDE:
         --------
         >>> # Prepare data
         >>> m_train = 100
-        >>> n_x, n_w = 1, 1
+        >>> n_x, n_y = 1, 1
         >>> x_train = np.random.normal(0, 1, size=(m_train, n_x))
-        >>> w_train = np.random.normal(0, 1, size=(m_train, n_w))
+        >>> y_train = np.random.normal(0, 1, size=(m_train, n_y))
         >>> weights_train = np.random.randint(1, 10, size=(m_train,))
-        >>> w_star = np.array([0.0] * n_w)
+        >>> y_star = np.array([0.0] * n_y)
         >>> bandwidth_x = np.array([0.5] * n_x)
-        >>> bandwidth_w = np.array([0.5] * n_w)
+        >>> bandwidth_y = np.array([0.5] * n_y)
         >>> # Fit the estimator
-        >>> params = (x_train, w_train, w_star, weights_train, bandwidth_x, bandwidth_w)
+        >>> params = (x_train, y_train, y_star, weights_train, bandwidth_x, bandwidth_y)
         >>> ckde = CKDE().fit(*params)
         """
         if x_train.ndim != 2:
@@ -102,20 +102,20 @@ class CKDE:
         self.m_train = self.x_train.shape[0]
         self.n_x = self.x_train.shape[1]
 
-        if w_train.ndim != 2:
-            raise ValueError("invalid shape of 'w_train' - should be 2d")
-        if w_train.shape[0] != x_train.shape[0]:
-            raise ValueError("invalid size of 'w_train'")
-        self.w_train = w_train
-        self.n_w = self.w_train.shape[1]
+        if y_train.ndim != 2:
+            raise ValueError("invalid shape of 'y_train' - should be 2d")
+        if y_train.shape[0] != x_train.shape[0]:
+            raise ValueError("invalid size of 'y_train'")
+        self.y_train = y_train
+        self.n_y = self.y_train.shape[1]
 
-        if w_star.ndim != 1:
-            raise ValueError("invalid shape of 'w_star' - should be 1d")
-        if w_star.shape[0] != self.n_w:
+        if y_star.ndim != 1:
+            raise ValueError("invalid shape of 'y_star' - should be 1d")
+        if y_star.shape[0] != self.n_y:
             raise ValueError(
-                f"invalid size of 'w_star'- should contain {self.n_w} values"
+                f"invalid size of 'y_star'- should contain {self.n_y} values"
             )
-        self.w_star = w_star
+        self.y_star = y_star
 
         if weights_train is None:
             self.weights_train = np.full(self.m_train, 1 / self.m_train)
@@ -128,8 +128,8 @@ class CKDE:
                 raise ValueError("'weights_train' should be positive")
             self.weights_train = weights_train / weights_train.sum()
 
-        if bandwidth_x is None or bandwidth_w is None:
-            z_train = np.concatenate((self.x_train, self.w_train), axis=1)
+        if bandwidth_x is None or bandwidth_y is None:
+            z_train = np.concatenate((self.x_train, self.y_train), axis=1)
             if bandwidth_method == "normal_reference":
                 bandwidth = normal_reference(z_train, self.kernel_name)
             elif bandwidth_method == "direct_plugin":
@@ -142,26 +142,26 @@ class CKDE:
             else:
                 raise ValueError("invalid 'bandwidth_method'")
             self.bandwidth_x = bandwidth[: self.n_x]
-            self.bandwidth_w = bandwidth[self.n_x :]
+            self.bandwidth_y = bandwidth[self.n_x :]
         else:
             if bandwidth_x.ndim != 1:
                 raise ValueError("invalid shape of 'bandwidth_x' - should be 1d")
-            if bandwidth_w.ndim != 1:
-                raise ValueError("invalid shape of 'bandwidth_w' - should be 1d")
+            if bandwidth_y.ndim != 1:
+                raise ValueError("invalid shape of 'bandwidth_y' - should be 1d")
             if bandwidth_x.shape[0] != self.n_x:
                 raise ValueError(
                     f"invalid size of 'bandwidth_x' - should contain {self.n_x} values"
                 )
-            if bandwidth_w.shape[0] != self.n_w:
+            if bandwidth_y.shape[0] != self.n_y:
                 raise ValueError(
-                    f"invalid size of 'bandwidth_w' - should contain {self.n_w} values"
+                    f"invalid size of 'bandwidth_y' - should contain {self.n_y} values"
                 )
             if not (bandwidth_x > 0).all():
                 raise ValueError("'bandwidth_x' should be positive")
-            if not (bandwidth_w > 0).all():
-                raise ValueError("'bandwidth_w' should be positive")
+            if not (bandwidth_y > 0).all():
+                raise ValueError("'bandwidth_y' should be positive")
             self.bandwidth_x = bandwidth_x
-            self.bandwidth_w = bandwidth_w
+            self.bandwidth_y = bandwidth_y
 
         self.fitted = True
         return self
@@ -189,14 +189,14 @@ class CKDE:
         --------
         >>> # Prepare data
         >>> m_train = 100
-        >>> n_x, n_w = 1, 1
+        >>> n_x, n_y = 1, 1
         >>> m_test = 10
         >>> x_train = np.random.normal(0, 1, (m_train, n_x))
-        >>> w_train = np.random.normal(0, 1, (m_train, n_w))
-        >>> w_star = np.array([0.0] * n_w)
+        >>> y_train = np.random.normal(0, 1, (m_train, n_y))
+        >>> y_star = np.array([0.0] * n_y)
         >>> x_test = np.random.uniform(-3, 3, (m_test, n_x))
         >>> # Fit the estimator.
-        >>> ckde = CKDE().fit(x_train, w_train, w_star)
+        >>> ckde = CKDE().fit(x_train, y_train, y_star)
         >>> # Compute pdf
         >>> scores, d = ckde.pdf(x_test)  # scores shape (10,)
         """
@@ -207,10 +207,10 @@ class CKDE:
             raise ValueError("invalid shape of 'x_test' - should be 2d")
 
         cond_weights_train = compute_d(
-            self.w_train,
+            self.y_train,
             self.weights_train,
-            self.w_star,
-            self.bandwidth_w,
+            self.y_star,
+            self.bandwidth_y,
             self.kernel_name,
         )
         scores = compute_kde(
