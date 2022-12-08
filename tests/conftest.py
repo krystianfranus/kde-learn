76fddf60030684a7a4f01fcd549cf998ec8732d0
Krystian Franus <krystian.franus@gmail.com>
2022-12-08 22:09:57 +0100

    Improve

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,9 +20,6 @@ def test_data():
 @pytest.fixture(scope="session")
 def classification_data():
     m_train, n = 100, 1
-    m_test = m_train
-
-    # Train data
     m_train1 = m_train // 2
     x_train1 = np.random.normal(0, 1, size=(m_train1, n))
     labels_train1 = np.full(m_train1, 1)
@@ -32,18 +29,7 @@ def classification_data():
     x_train = np.concatenate((x_train1, x_train2))
     labels_train = np.concatenate((labels_train1, labels_train2))
     weights_train = np.full((m_train,), 1 / m_train)
-
-    # Test data
-    m_test1 = m_test // 2
-    x_test1 = np.random.normal(0, 1, size=(m_test1, n))
-    labels_test1 = np.full(m_test1, 1)
-    m_test2 = m_test // 2
-    x_test2 = np.random.normal(3, 1, size=(m_test2, n))
-    labels_test2 = np.full(m_test2, 2)
-    x_test = np.concatenate((x_test1, x_test2))
-    labels_test = np.concatenate((labels_test1, labels_test2))
-
-    return x_train, labels_train, weights_train, x_test, labels_test
+    return x_train, labels_train, weights_train
 
 
 @pytest.fixture(scope="session")
